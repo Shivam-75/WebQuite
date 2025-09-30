370652b810515dc5dfdced65fea29f90e3d98a9e
Shivam-75 <shivampandey7518200305@gmail.com>
2025-09-30 12:55:55 +0530

    change Style and font

--- frontend/src/components/admin/LoginOrnat.jsx
+++ frontend/src/components/admin/LoginOrnat.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { useStore } from '../../store/Auth'
-import { Outlet, useNavigate } from 'react-router-dom';
+import React from "react";
+import { useStore } from "../../store/Auth";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const LoginOrnat = () => {
-    const navigate = useNavigate();
-    const { profilename, isLogin } = useStore();
-    if (profilename || isLogin) {
-      return (
-        <>
-          <Outlet />
-        </>
-      );
-    } else {
-      navigate("/api/user/login") 
-    }
+  const navigate = useNavigate();
+  const { profilename, isLogin } = useStore();
+  if (profilename || isLogin) {
+    return (
+      <>
+        <Outlet />
+      </>
+    );
+  } else {
+    navigate("/api/user/login");
+  }
+};
 
-}
-
-export default LoginOrnat
+export default LoginOrnat;
